4e4c26844d188c5cca7b8f667c1cf47212753234
OuqasM <ouqas.pro@gmail.com>
2023-08-25 17:50:27 +0100

    Add Filter by Category

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
     <h2>Product List</h2>
+    <div>
+      <label for="categorySelect">Filter by Category:</label>
+      <select id="categorySelect" v-model="selectedCategoryId" @change="getFilteredProducts">
+        <option value="">All Categories</option>
+        <option v-for="category in categories" :value="category.id">{{ category.name }}</option>
+      </select>
+    </div>
     <div class="row">
       <div class="card col-md-4 p-1 m-3" v-for="product in products.products" :key="product.id">
         <div class="product-item">
@@ -28,12 +35,35 @@ export default {
     return {
       products: [],
       host : "http://127.0.0.1:8000/",
+      categories: [], 
+      selectedCategoryId: '' 
     };
   },
+  methods: {
+    async getFilteredProducts() {
+
+      console.log("salam");
+      try {
+        const response = await axios.get('http://127.0.0.1:8000/api/v1/products', {
+          params: {
+            category_id: this.selectedCategoryId
+          }
+        });
+        this.products = response.data;
+      } catch (error) {
+        console.error('Error fetching products:', error);
+      }
+    }
+    },
   async mounted() {
     try {
       const response = await axios.get('http://127.0.0.1:8000/api/v1/products');
       this.products = response.data;
+
+      // Fetch categories and populate the categories array
+      const categoriesResponse = await axios.get('http://127.0.0.1:8000/api/v1/categories');
+      this.categories = categoriesResponse.data.categories;
+
     } catch (error) {
       console.error('Error fetching products:', error);
     }
